4e4edd59ad39502ca3681696e1794ddf380fe7a2
Everton Oliveira <evertonwallace@consite.com.br>
2023-10-24 22:43:31 -0300

    getters and setters productModel

--- src/main/java/com/example/produtos/models/ProductModel.java
+++ src/main/java/com/example/produtos/models/ProductModel.java
@@ -21,5 +21,25 @@ public class ProductModel implements Serializable {
 	private UUID idProduct;
 	private String name;
 	private BigDecimal value;
-
+	
+	public UUID getIdProduct() {
+		return idProduct;
+	}
+	public void setIdProduct(UUID idProduct) {
+		this.idProduct = idProduct;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public BigDecimal getValue() {
+		return value;
+	}
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+	
+	
 }
