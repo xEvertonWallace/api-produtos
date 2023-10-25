21086eebfb91f3bb7443e2ee955f94f3a27ae5fe
Everton Oliveira <evertonwallace@consite.com.br>
2023-10-24 23:30:20 -0300

    Criação da classe ProductRepository

---
+++ src/main/java/com/example/produtos/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.example.produtos.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.produtos.models.ProductModel;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
+}
