74edce023fd171ecf720683b96c8cea6d7f513de
Everton Oliveira <evertonwallace@consite.com.br>
2023-10-29 10:37:50 -0300

    Criancao dos metodos getAll e getOne na classe ProductController

--- src/main/java/com/example/produtos/controller/ProductController.java
+++ src/main/java/com/example/produtos/controller/ProductController.java
@@ -1,9 +1,15 @@
 package com.example.produtos.controller;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +35,26 @@ public class ProductController {
 		BeanUtils.copyProperties(productRecordDto, productModel);
 		//se ocorreu bem ele retorna o status created no corpo(body) retorna o que foi criado e salva na base de dados pelo productRepository
 		return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+	}
+	
+	@GetMapping("/products")
+	public ResponseEntity<List<ProductModel>> getAllProducts(){
+		//o retorno será um response entity, porém no corpo tera uma lista de produtos
+		return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+	}
+	
+	//apresenta um unico produto passando o id
+	@GetMapping("/products/{id}")
+	public ResponseEntity<Object> getOneProducts(@PathVariable(value="id") UUID id){
+		//percorre a nase de dados para verificar se existe esse id
+		Optional<ProductModel> productID = productRepository.findById(id);
+		//se for vazio nao encontrado
+		if(productID.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("product not found.");
+		}
+		//se nao return o product
+		return ResponseEntity.status(HttpStatus.OK).body(productID.get());
+		
+		
 	}
 }
